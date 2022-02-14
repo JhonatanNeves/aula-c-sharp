0c8e53a4f09603d7a3992967be22c62e9df43e70
JhonatanNeves <nevesjn2@gmail.com>
2022-02-14 20:23:26 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int numeroDeVezes = 51;
+
+for (int i=1; i < numeroDeVezes; i++){
+    Console.WriteLine($"Bem Vindo {i}");
+}
